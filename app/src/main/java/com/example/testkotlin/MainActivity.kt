279f8c930703a4cef16cc7f03ac36f54d72b87b8
wxt <602574436@qq.com>
2021-10-21 14:23:53 +0800

    gradle

--- app/src/main/java/com/example/testkotlin/MainActivity.kt
+++ app/src/main/java/com/example/testkotlin/MainActivity.kt
@@ -3,10 +3,11 @@
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 
-  const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
+const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +20,6 @@ class MainActivity : AppCompatActivity() {
 //            putExtra(EXTRA_MESSAGE, message)
 //        }
 //        startActivity(intent)
+        Log.d("************", message)
     }
 }
